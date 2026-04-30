02eed1e3f9ac739293b31e9a3bf94a9b2ba56f2e
andre <andremendespr@gmail.com>
2026-04-30 04:23:01 -0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,46 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Plus, List, X, Check, SquarePen } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 
 const Home = () => {
   return (
     <main className="w-full h-screen flex justify-center items-center p-24 bg-linear-to-b from-slate-950 to-slate-600 min-h-screen">
-      <Card>
-        {" "}
-        <Card className="w-lg">
-          <CardHeader className="gap-2 grid grid-cols-2">
-            <CardTitle className="text-center col-span-3">To do list</CardTitle>
-            <Input className="col-span-2" placeholder="Digite sua tarefa" />
-            <Button variant="destructive" className="cursor-pointer ">
-              <Plus /> Cadastrar
-            </Button>
-          </CardHeader>
-          <CardContent>
-            <Separator className="my-4" />
+      <Card className="w-lg shadow-lg shadow-pink-700">
+        <CardHeader className="gap-2 grid grid-cols-2">
+          <CardTitle className="text-center col-span-3">To do list</CardTitle>
+          <Input className="col-span-2" placeholder="Digite sua tarefa" />
+          <Button variant="destructive" className="cursor-pointer ">
+            <Plus /> Cadastrar
+          </Button>
+        </CardHeader>
 
-            <Badge>todos</Badge>
-          </CardContent>
-          <CardFooter></CardFooter>
-        </Card>
+        <CardContent>
+          <Separator />
+          <div className="flex gap-1 ">
+            <Badge className="cursor-pointer bg-pink-600">
+              <List />
+              todos
+            </Badge>
+            <Badge className="cursor-pointer" variant="outline">
+              <X /> Não finalizadas
+            </Badge>
+            <Badge className="cursor-pointer" variant="secondary">
+              <Check /> Concluídas
+            </Badge>
+          </div>
+        </CardContent>
+
+        <div className="mt-4 border-b">
+          <div className="flex items-center h-14 border-t">
+            <div className="h-full w-1 bg-fuchsia-800"></div>
+            <SquarePen />
+            <p className="flex-1 px-2 text-sm"> Te amo Ana</p>
+          </div>
+        </div>
+        <CardFooter></CardFooter>
       </Card>
     </main>
   );
